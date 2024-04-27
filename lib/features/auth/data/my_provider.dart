daaa2f6ad42fa85362a073f2b6f06ab7fdd1419f
YoussefAGS <ya023560@gmail.com>
2024-04-27 15:52:59 +0300

    add provider

---
+++ lib/features/auth/data/my_provider.dart
@@ -0,0 +1,21 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../../core/utils/database_uitles.dart';
+import 'model/user.dart';
+
+class MyProvider extends ChangeNotifier{
+  MyUser? myUser;
+  User? firebaseUser;
+  MyProvider(){
+  firebaseUser= FirebaseAuth.instance.currentUser;
+  if(firebaseUser!=null){
+    initUser();
+  }
+  }
+  void initUser()async{
+    myUser=await DatabaseUtils.readUserFormFirebase(firebaseUser?.uid??"");
+  }
+
+}
